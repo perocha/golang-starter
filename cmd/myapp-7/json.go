d8921825cac8a609f2f90439dd370d6984da9436
procha <pedrojrocha@outlook.com>
2022-05-22 10:27:52 +0200

    Basic example json Marshal & Unmarshal functions

---
+++ cmd/myapp-7/json.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Person struct {
+	ID        int
+	FirstName string `json:"name"`
+	LastName  string
+	Address   string `json:"address,omitempty"`
+}
+
+type Employee struct {
+	Person
+	ManagerID int
+}
+
+type Contractor struct {
+	Person
+	CompanyID int
+}
+
+func main() {
+	employeeList := []Employee{
+		Employee{
+			Person: Person{
+				LastName: "Doe", FirstName: "John",
+			},
+		},
+		Employee{
+			Person: Person{
+				LastName: "Campbell", FirstName: "David",
+			},
+		},
+	}
+
+	jsonData, _ := json.Marshal(employeeList)
+	fmt.Printf("%s\n", jsonData)
+
+	var decoded []Employee
+	json.Unmarshal(jsonData, &decoded)
+	fmt.Printf("%v", decoded)
+}
